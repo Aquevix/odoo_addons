a62a4b59412a058bfbc8fb5f3147920cb42c96f6
priyankagarg-python <59206515+priyankagarg-python@users.noreply.github.com>
2020-06-09 15:09:51 +0530

    Added setup.py file.

---
+++ smile_redis_session_store/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='smile_redis_session_store',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/Aquevix/odoo_addons/tree/12.0/smile_redis_session_store',
+    license='AGPL-3',
+    author='Aquevix (original: smile)',
+    author_email='',
+    description='Customized module for internal users timeout with redis session'
+)
